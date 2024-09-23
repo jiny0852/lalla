8ab991bc65cfdf85a1b049af273f50692653aaa1
신지연 <jiny0852@gmail.com>
2024-09-23 18:25:15 +0900

    2024-09-23

---
+++ src/views/Ex02.jsx
@@ -0,0 +1,21 @@
+//import 라이브러리
+
+import React from 'react';
+
+
+const Ex02 = () => {
+
+
+    return (
+
+        <>
+
+            <p>야이야아ㅣ아이ㅏㅇㅇ이ㅏ</p>
+
+        </>
+
+    );
+
+}
+
+export default Ex02;
